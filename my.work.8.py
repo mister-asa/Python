8078368d57f9fed8d2def13f58bccc4e9b533a16
afro <rusalpe@gmail.com>
2022-04-03 22:16:57 +0300

    решил задание 8 на питоне

---
+++ my.work.8.py
@@ -0,0 +1,240 @@
+# 1
+class Data:
+    def __init__(self, day_month_year):
+        # self.day = day
+        # self.month = month
+        # self.year = year
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-': my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2019 >= year >= 0:
+                    return f'All right'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+today = Data('11 - 1 - 2001')
+print(today)
+print(Data.valid(11, 11, 2022))
+print(today.valid(11, 13, 2011))
+print(Data.extract('11 - 11 - 2011'))
+print(today.extract('11 - 11 - 2020'))
+print(Data.valid(1, 11, 2000))
+
+
+
+
+
+# 2
+class MyException(Exception):
+
+    def division_func(self, a, b):
+        try:
+            res = round(a / b, 2)
+        except ZeroDivisionError:
+            print(f"{a} / {b} -> на ноль делить нельзя!\n")
+        else:
+            print(f"{a} / {b} = {res} \n")
+
+
+m_e = MyException()
+
+m_e.division_func(1, 2)
+m_e.division_func(1, 0)
+m_e.division_func(-1, 3)
+m_e.division_func(0, 4)
+
+
+
+
+# 3
+class OwnError(Exception):
+
+    def func_str(self, my_list):
+        try:
+            for el in my_list:
+                if type(el) == str:
+                    raise OwnError("в списке присутствует элемент типа данных <<str>>: ")
+
+        except OwnError as err:
+            print(err, el)
+
+    def func_bool(self, my_list):
+        try:
+            for el in my_list:
+                if type(el) == bool:
+                    raise OwnError("в списке присутствует элемент типа данных <<bool>>: ")
+        except OwnError as err:
+            print(err, el)
+
+
+input_list = [2, 1.2, 'str-элемент', bool(20)]
+print("Введенный список: ", input_list, "\n")
+my_err = OwnError()
+my_err.func_str(input_list)
+my_err.func_bool(input_list)
+
+
+
+
+
+# 4
+
+class OfficeEquipmentWarehouse:
+    """Класс, описывающий склад оргтехники"""
+    print("\nСклад оргтехники")
+
+
+class OfficeEquipment:
+    """Базовый класс оргтехники"""
+    def __init__(self, producer, color):
+        self.producer = producer
+        self.color = color
+
+
+class Printer(OfficeEquipment):
+    """Класс принтер"""
+    amount_pr = 0
+
+    def __init__(self, producer, color, pr_type):
+        super().__init__(producer, color)
+        self.pr_type = pr_type
+        Printer.amount_pr += 1
+
+    @staticmethod
+    def name():
+        return "<<Принтер>>"
+
+    def type_printer(self):
+        return self.pr_type
+
+    def __str__(self):
+        return "\tпроизводитель: {} \tцвет: {}  \tтип принтера: {}".format(self.producer, self.color, self.pr_type)
+
+
+class Scanner(OfficeEquipment):
+    """Класс сканер"""
+    amount_sc = 0
+
+    def __init__(self, producer, color, sc_sensor):
+        super().__init__(producer, color)
+        self.sc_sensor = sc_sensor
+        Scanner.amount_sc += 1
+
+    @staticmethod
+    def name():
+        return"<<Сканер>>"
+
+    def type_sensor(self):
+        return self.sc_sensor
+
+    def __str__(self):
+        return "\tпроизводитель: {} \tцвет: {} \tтип сенсора: {}".format(self.producer, self.color, self.sc_sensor)
+
+
+class Xerox(OfficeEquipment):
+    """Класс ксерокс"""
+    amount_x = 0
+
+    def __init__(self, producer, color, xer_wi_fi):
+        super().__init__(producer, color)
+        self.xer_wi_fi = xer_wi_fi
+        Xerox.amount_x += 1
+
+    @staticmethod
+    def name():
+        return "<<Ксерокс>>"
+
+    def wi_fi_module(self):
+        return self.xer_wi_fi
+
+    def __str__(self):
+        return "\tпроизводитель: {} \tцвет: {}   \tWi-Fi модуль: {}".format(self.producer, self.color, self.xer_wi_fi)
+
+
+p = Printer('Canon', 'white', 'струйный')
+p2 = Printer('Brother', 'blue', 'лазерный')
+print(p.name(), p.amount_pr, "шт")
+print(p.__str__())
+print(p2.__str__())
+
+
+s = Scanner('Epson', 'black', 'CIS')
+s2 = Scanner('Avision', 'white', 'CCD')
+s3 = Scanner('Kodak', 'yellow', 'CMOS')
+print(s.name(), s.amount_sc, "шт")
+print(s.__str__())
+print(s2.__str__())
+print(s3.__str__())
+
+
+x = Xerox('Hanp', 'white', 'есть')
+x2 = Xerox('Phaser', 'black', 'есть')
+x3 = Xerox('Xerox', 'white', 'есть')
+x4 = Xerox('Pantum', 'red', 'отсутствует')
+print(x.name(), x.amount_x, "шт")
+print(x.__str__())
+print(x2.__str__())
+print(x3.__str__())
+print(x4.__str__())
+
+
+
+
+
+
+# 7
+import math
+
+
+class MyComplex:
+
+     def __init__(self, a, b):
+         self.a = a
+         self.b = b
+
+     def __str__(self):
+         return '(%s+%sj)' % (self.a, self.b)
+
+     def __add__(self, other):
+         new_a = self.a + other.a
+         new_b = self.b + other.b
+         return MyComplex(new_a, new_b)
+
+     def __mul__(self, other):
+         new_a = self.a * other.a - self.b * other.b
+         new_b = self.b * other.a + self.a * other.b
+         return MyComplex(new_a, new_b)
+
+
+z1 = MyComplex(1, 2)
+z2 = MyComplex(2, 3)
+
+print(f"{z1} + {z2} = ", z1 + z2)
+print(f"{z1} * {z2} = ", z1 * z2)
+
+# проверка
+# print(complex(1, 2)+complex(2, 3))
+# print(complex(1, 2)*complex(2, 3))
+
+
